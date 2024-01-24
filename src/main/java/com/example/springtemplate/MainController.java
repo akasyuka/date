56e0488ba6c3bfce7383826bab101e9c4d6cf3fe
Aleksandr Kasyuk <aleksandrkasyuk@gmail.com>
2024-01-25 00:12:46 +0300

    Адаптивная верстка

--- src/main/java/com/example/springtemplate/MainController.java
+++ src/main/java/com/example/springtemplate/MainController.java
@@ -1,17 +1,20 @@
 package com.example.springtemplate;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
 
     @GetMapping("/")
-    public String hello() {
-        return "index";
+    public String main() {
+        return "main";
+    }
+
+    @GetMapping("/react")
+    public String react() {
+        return "react";
     }
 }
 
